b3c8d703464942b6391c22440d346019108013a4
ketebekk <bekkerman.ed@drom.ru>
2022-02-17 14:41:46 +1000

    Работает отображение фото в ViewControllerSubClassedCell и удаление ячеек при прикосновении, есть pull-to-refresh

---
+++ TestTakFirstTry/Model/Profile.swift
@@ -0,0 +1,13 @@
+//
+//  Profile.swift
+//  TestTakFirstTry
+//
+//  Created by bekkerman on 06.02.2022.
+//
+
+import Foundation
+
+struct Profile {
+    let name: String
+    let imageName: String
+}
